3245e2e5baf5be2d98495e72f458687bca64f09a
Tiago Gomes <tacg@tacgomes.com>
2020-12-01 14:09:53 +0000

    Add solutions to day 1

---
+++ puzzle-1a.rs
@@ -0,0 +1,22 @@
+use std::io::{prelude::*, BufReader};
+use std::fs::File;
+use std::collections::HashSet;
+
+const TARGET : i32 = 2020;
+
+fn main() {
+    let f = File::open("input.txt").unwrap();
+    let lines = BufReader::new(f).lines();
+
+    let mut set = HashSet::new();
+
+    for line in lines {
+        let n = line.unwrap().parse::<i32>().unwrap();
+        let diff = TARGET - n;
+        if set.contains(&diff) {
+            println!("Result: {}", diff * n);
+            break;
+        }
+        set.insert(n);
+    }
+}
---
+++ puzzle-1b.rs
@@ -0,0 +1,32 @@
+use std::io::{prelude::*, BufReader};
+use std::fs::File;
+use std::collections::HashSet;
+
+const TARGET : i32 = 2020;
+
+fn main() {
+    let f = File::open("input.txt").unwrap();
+    let lines = BufReader::new(f).lines();
+
+    let mut vec = vec![];
+    let mut set = HashSet::new();
+
+    for line in lines {
+        let n = line.unwrap().parse::<i32>().unwrap();
+        vec.push(n);
+        set.insert(n);
+    }
+
+    // Assumes non-repeated elements
+    assert_eq!(set.len(), vec.len());
+
+    'outer: for (a_i, a) in vec[..vec.len() - 2].iter().enumerate() {
+        for b in vec[a_i + 1..].iter() {
+            let diff = TARGET - a - b;
+            if set.contains(&diff) {
+                println!("Result: {}", a * b * diff);
+                break 'outer;
+            }
+        }
+    }
+}
