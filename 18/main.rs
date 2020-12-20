583392878998bc1ca08b1f662aecdf719413cb62
Tiago Gomes <tacg@tacgomes.com>
2020-12-20 16:42:20 +0000

    Add solutions for day 18 - part 2

--- 18/main.rs
+++ 18/main.rs
@@ -3,12 +3,18 @@ use std::fs;
 use std::path::Path;
 use std::process;
 
-fn calculate_sum(line: &[char]) -> usize {
-    let add = |x: usize, y: usize| -> usize { x + y };
-    let mul = |x: usize, y: usize| -> usize { x * y };
+#[derive(Eq, PartialEq)]
+enum StackType {
+    Parens,
+    Mult,
+}
+
+fn calculate_sum_part1(line: &[char]) -> usize {
+    let add: fn(usize, usize) -> usize = |x: usize, y: usize| -> usize { x + y };
+    let mul: fn(usize, usize) -> usize = |x: usize, y: usize| -> usize { x * y };
 
     let mut num = 0;
-    let mut op: fn(usize, usize) -> usize = add;
+    let mut op = add;
     let mut stack = vec![];
 
     for c in line {
@@ -36,12 +42,63 @@ fn calculate_sum(line: &[char]) -> usize {
     num
 }
 
-fn calculate_all_sums(file_name: impl AsRef<Path>) -> usize {
+fn calculate_sum_part2(line: &[char]) -> usize {
+    let mut num = 0;
+    let mut stack = vec![];
+
+    for c in line {
+        match c {
+            '0'..='9' => {
+                let new_num = c.to_digit(10).unwrap() as usize;
+                num += new_num;
+            }
+            '+' => (),
+            '*' => {
+                if let Some((n, StackType::Mult)) = stack.last() {
+                    num *= n;
+                    stack.pop();
+                }
+                stack.push((num, StackType::Mult));
+                num = 0;
+            }
+            '(' => {
+                stack.push((num, StackType::Parens));
+                num = 0;
+            }
+            ')' => {
+                if let Some((n, StackType::Mult)) = stack.last() {
+                    num *= n;
+                    stack.pop();
+                }
+                let (prev_num, _stype) = stack.pop().unwrap();
+                num += prev_num;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    if let Some((m, StackType::Mult)) = stack.pop() {
+        num *= m;
+    }
+
+    num
+}
+
+fn calculate_all_sums_part1(file_name: impl AsRef<Path>) -> usize {
     let content = fs::read_to_string(file_name).unwrap();
     content
         .trim()
         .split('\n')
-        .map(|x| calculate_sum(&x.chars().filter(|&c| c != ' ').collect::<Vec<_>>()))
+        .map(|x| calculate_sum_part1(&x.chars().filter(|&c| c != ' ').collect::<Vec<_>>()))
+        .sum()
+}
+
+fn calculate_all_sums_part2(file_name: impl AsRef<Path>) -> usize {
+    let content = fs::read_to_string(file_name).unwrap();
+    content
+        .trim()
+        .split('\n')
+        .map(|x| calculate_sum_part2(&x.chars().filter(|&c| c != ' ').collect::<Vec<_>>()))
         .sum()
 }
 
@@ -51,8 +108,10 @@ fn main() {
         process::exit(1);
     }
 
-    let count = calculate_all_sums(env::args().nth(1).unwrap());
-    println!("Result: {}", count);
+    let res1 = calculate_all_sums_part1(env::args().nth(1).unwrap());
+    let res2 = calculate_all_sums_part2(env::args().nth(1).unwrap());
+    println!("Result (Part 1): {}", res1);
+    println!("Result (Part 1): {}", res2);
 }
 
 #[cfg(test)]
@@ -61,21 +120,25 @@ mod tests {
 
     #[test]
     fn test_example_input_1() {
-        assert_eq!(calculate_all_sums("example1.txt"), 71);
+        assert_eq!(calculate_all_sums_part1("example1.txt"), 71);
+        assert_eq!(calculate_all_sums_part2("example1.txt"), 231);
     }
 
     #[test]
     fn test_example_input_2() {
-        assert_eq!(calculate_all_sums("example2.txt"), 51);
+        assert_eq!(calculate_all_sums_part1("example2.txt"), 51);
+        assert_eq!(calculate_all_sums_part2("example2.txt"), 51);
     }
 
     #[test]
     fn test_example_input_3() {
-        assert_eq!(calculate_all_sums("example3.txt"), 26335);
+        assert_eq!(calculate_all_sums_part1("example3.txt"), 26335);
+        assert_eq!(calculate_all_sums_part2("example3.txt"), 693891);
     }
 
     #[test]
     fn test_puzzle_input() {
-        assert_eq!(calculate_all_sums("input.txt"), 3647606140187);
+        assert_eq!(calculate_all_sums_part1("input.txt"), 3647606140187);
+        assert_eq!(calculate_all_sums_part2("input.txt"), 323802071857594);
     }
 }
