e88fc74876a567f40057800ca05986f3230c4c07
Tiago Gomes <tacg@tacgomes.com>
2020-12-30 12:18:33 +0000

    Lay some more groundwork for day 20

--- 20/main.rs
+++ 20/main.rs
@@ -3,36 +3,108 @@ use std::fs;
 use std::path::Path;
 use std::process;
 
-struct _Tile {
+const TOP: usize = 0;
+const RIGHT: usize = 1;
+const BOTTOM: usize = 2;
+const LEFT: usize = 3;
+
+struct Tile {
     tile_id: usize,
-    borders: [Vec<char>; 4],
+    alignments: Vec<Vec<String>>,
+    current_alignment: usize,
+}
+
+impl Tile {
+    fn new(tile_id: usize, borders: Vec<String>) -> Self {
+        let mut alignments = vec![];
+
+        // println!("\ntileid: {}", tile_id);
+        // println!("borders:  {:?}", borders);
+
+        alignments.push(borders.clone());
+
+        let vertically_mirrored = vec![
+            borders[BOTTOM].clone(),
+            borders[RIGHT].chars().rev().clone().collect(),
+            borders[TOP].clone(),
+            borders[LEFT].chars().rev().clone().collect(),
+        ];
+
+        // println!("vertical: {:?}", vertically_mirrored);
+
+        alignments.push(vertically_mirrored.clone());
+
+        for border in [&borders, &vertically_mirrored].iter() {
+            let mut border = border.to_vec();
+            for _ in 0..3 {
+                border = vec![
+                    border[LEFT].chars().rev().clone().collect(),
+                    border[TOP].clone(),
+                    border[RIGHT].chars().rev().clone().collect(),
+                    border[BOTTOM].clone(),
+                ];
+                // println!("rotation: {:?}", border);
+                alignments.push(border.clone());
+            }
+        }
+
+        Tile {
+            tile_id,
+            alignments,
+            current_alignment: 0,
+        }
+    }
+
+    fn next_alignment(&mut self) {
+        self.current_alignment += 1;
+        self.current_alignment %= self.alignments.len();
+    }
+
+    fn top(&self) -> &str {
+        return &self.alignments[self.current_alignment][TOP];
+    }
+
+    fn right(&self) -> &str {
+        return &self.alignments[self.current_alignment][RIGHT];
+    }
+
+    fn bottom(&self) -> &str {
+        return &self.alignments[self.current_alignment][BOTTOM];
+    }
+
+    fn left(&self) -> &str {
+        return &self.alignments[self.current_alignment][LEFT];
+    }
+}
+
+fn find_alignment(_tiles: &mut Vec<Tile>) -> Vec<Vec<&Tile>> {
+    let mut _array = vec![];
+    _array
 }
 
 fn calculate_part1(file_name: impl AsRef<Path>) -> usize {
     let content = fs::read_to_string(file_name).unwrap();
-    let tiles: Vec<_> = content.split("\n\n").collect();
+    let blocks: Vec<_> = content.trim().split("\n\n").collect();
 
-    for tile in tiles {
+    let mut tiles = vec![];
+
+    for tile in blocks {
         let mut lines = tile.split('\n');
         let tile_id = &lines.next().unwrap();
         let tile_id = &tile_id[5..tile_id.len() - 1].parse::<usize>().unwrap();
-        println!("\ntileid: {}", tile_id);
 
-        let mut borders = vec![vec![]; 4];
-
-        borders[0] = lines.clone().next().unwrap().chars().collect::<Vec<_>>();
-        borders[2] = lines.clone().last().unwrap().chars().collect::<Vec<_>>();
+        let mut borders = vec![String::new(); 4];
+        borders[0] = lines.clone().next().unwrap().to_string();
+        borders[2] = lines.clone().last().unwrap().to_string();
         for line in lines {
             borders[3].push(line.chars().next().unwrap());
             borders[1].push(line.chars().last().unwrap());
         }
 
-        println!("top: {:?}", borders[0]);
-        println!("right: {:?}", borders[1]);
-        println!("bottom: {:?}", borders[2]);
-        println!("left: {:?}", borders[3]);
+        tiles.push(Tile::new(*tile_id, borders));
     }
 
+    let _array = find_alignment(&mut tiles);
     0
 }
 
