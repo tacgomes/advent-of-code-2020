c917e09890718a18c9e7b165f80e1d3551ce25c3
Tiago Gomes <tacg@tacgomes.com>
2021-01-09 17:43:40 +0000

    Day 03: improve solution

--- 03/main.rs
+++ 03/main.rs
@@ -1,95 +1,55 @@
 use std::collections::HashSet;
 use std::env;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::fs;
 use std::path::Path;
 use std::process;
 
-#[derive(PartialEq, Eq, Hash)]
-struct MapPosition {
-    r: usize,
-    c: usize,
-}
-
-impl MapPosition {
-    fn new(r: usize, c: usize) -> Self {
-        MapPosition { r, c }
-    }
-
-    fn advance(&mut self, movement: &Self) {
-        self.r += movement.r;
-        self.c += movement.c;
-    }
-}
+type Point = (usize, usize);
 
 struct Map {
-    num_rows: usize,
-    num_cols: usize,
-    trees: HashSet<MapPosition>,
+    ncols: usize,
+    trees: HashSet<Point>,
 }
 
 impl Map {
-    fn new(num_rows: usize, num_cols: usize) -> Self {
-        Map {
-            num_rows,
-            num_cols,
-            trees: HashSet::new(),
-        }
-    }
-
-    fn add_tree(&mut self, point: MapPosition) {
-        self.trees.insert(point);
+    fn new(ncols: usize, trees: HashSet<Point>) -> Self {
+        Map { ncols, trees }
     }
 
     fn count_trees_part1(&self) -> usize {
-        self.navigate_toboggan(&MapPosition::new(1, 3))
+        self.count_trees(&(1, 3))
     }
 
     fn count_trees_part2(&self) -> usize {
-        [
-            MapPosition::new(1, 1),
-            MapPosition::new(1, 3),
-            MapPosition::new(1, 5),
-            MapPosition::new(1, 7),
-            MapPosition::new(2, 1),
-        ]
-        .iter()
-        .map(|m| self.navigate_toboggan(m))
-        .product()
+        [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+            .iter()
+            .map(|m| self.count_trees(m))
+            .product()
     }
 
-    fn navigate_toboggan(&self, movement: &MapPosition) -> usize {
-        let mut num_trees = 0;
-        let mut current_pos = MapPosition::new(0, 0);
-
-        while current_pos.r < self.num_rows {
-            current_pos.advance(&movement);
-            current_pos.c %= self.num_cols;
-
-            if self.trees.contains(&current_pos) {
-                num_trees += 1;
-            }
-        }
-
-        num_trees
+    fn count_trees(&self, mov: &Point) -> usize {
+        self.trees
+            .iter()
+            .filter(|t| t.0 % mov.0 == 0 && t.1 == (t.0 / mov.0 * mov.1) % self.ncols)
+            .count()
     }
 }
 
 fn parse_input(file_name: impl AsRef<Path>) -> Map {
-    let file = File::open(file_name).unwrap();
-    let lines = BufReader::new(file).lines();
-    let lines: Vec<_> = lines.map(|x| x.unwrap()).collect();
+    let content = fs::read_to_string(file_name).unwrap();
 
-    let mut map = Map::new(lines.len(), lines[0].chars().count());
+    let trees = content
+        .lines()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|&(_, ch)| ch == '#')
+                .map(move |(col, _)| (row, col))
+        })
+        .collect();
 
-    lines.iter().enumerate().for_each(|(row, line)| {
-        line.chars()
-            .enumerate()
-            .filter(|&(_, ch)| ch == '#')
-            .for_each(|(col, _)| map.add_tree(MapPosition::new(row, col)))
-    });
-
-    map
+    Map::new(content.lines().next().unwrap().chars().count(), trees)
 }
 
 fn main() {
