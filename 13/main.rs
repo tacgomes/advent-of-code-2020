67d53e8a6ade9ce789d1f2d0925f0e55030c8f68
Tiago Gomes <tacg@tacgomes.com>
2021-01-10 18:20:51 +0000

    Day 13: improve solution

--- 13/main.rs
+++ 13/main.rs
@@ -1,83 +1,60 @@
 use std::env;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::fs;
 use std::path::Path;
 use std::process;
 
-struct BusSchedule {
-    earliest_ts: usize,
-    bus_ids: Vec<usize>,
-    departing_offsets: Vec<(usize, isize)>,
+fn solve_part1(timestamp: usize, schedule: &[(usize, isize)]) -> usize {
+    let mut timestamps = schedule
+        .iter()
+        .map(|(x, _)| (x, (x - (timestamp % x) % x) + timestamp))
+        .collect::<Vec<_>>();
+    timestamps.sort_by_key(|&(_, ts)| ts);
+    (timestamps[0].1 - timestamp) * timestamps[0].0
 }
 
-impl BusSchedule {
-    fn from_file(file_name: impl AsRef<Path>) -> Self {
-        let file = File::open(file_name).unwrap();
-        let mut lines = BufReader::new(file).lines();
+fn solve_part2_brute_force(schedule: &[(usize, isize)]) -> Option<isize> {
+    // TODO: add an implementation of part 2 using the much more
+    // efficient Chinese Remainder Theorem.
 
-        let mut bus_ids = vec![];
-        let mut departing_offsets = vec![];
+    let highest_bus_id = schedule.iter().map(|(x, _)| x).max().unwrap();
+    let highest_bus_offset = schedule
+        .iter()
+        .find(|(x, _)| x == highest_bus_id)
+        .unwrap()
+        .1;
 
-        let earliest_ts = lines.next().unwrap().unwrap().parse().unwrap();
+    let mut schedule = schedule.to_owned();
+    for (_, offset) in schedule.iter_mut() {
+        *offset -= highest_bus_offset as isize;
+    }
 
-        for (index, bus_id) in lines
-            .next()
-            .unwrap()
-            .unwrap()
-            .split(',')
-            .enumerate()
-            .filter(|(_, x)| !x.starts_with('x'))
+    for n in (0isize..).step_by(*highest_bus_id) {
+        if schedule
+            .iter()
+            .all(|(bus_id, offset)| (n + offset) % *bus_id as isize == 0)
         {
-            let bus_id = bus_id.parse().unwrap();
-            bus_ids.push(bus_id);
-            departing_offsets.push((bus_id, index as isize));
-        }
-
-        BusSchedule {
-            earliest_ts,
-            bus_ids,
-            departing_offsets,
+            return Some(n + schedule[0].1);
         }
     }
 
-    fn part1(&self) -> usize {
-        let mut timestamps: Vec<_> = self
-            .bus_ids
-            .iter()
-            .map(|x| (x, (x - (self.earliest_ts % x) % x) + self.earliest_ts))
-            .collect();
-        timestamps.sort_by_key(|&(_, ts)| ts);
-        (timestamps[0].1 - self.earliest_ts) * timestamps[0].0
-    }
+    None
+}
 
-    fn part2_brute_force(&self) -> Option<isize> {
-        // TODO: add an implementation of part 2 using the much more
-        // efficient Chinese Remainder Theorem.
+fn parse_input(file_name: impl AsRef<Path>) -> (usize, Vec<(usize, isize)>) {
+    let content = fs::read_to_string(&file_name).unwrap();
+    let mut lines = content.lines();
 
-        let highest_bus_id = self.bus_ids.iter().max().unwrap();
-        let highest_bus_offset = self
-            .departing_offsets
-            .iter()
-            .find(|(x, _)| x == highest_bus_id)
-            .unwrap()
-            .1;
+    let timestamp = lines.next().unwrap().parse().unwrap();
+    let schedule = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .enumerate()
+        .filter(|(_, b)| !b.starts_with('x'))
+        .map(|(i, b)| (b.parse().unwrap(), i as isize))
+        .collect();
 
-        let mut departing_offsets = self.departing_offsets.clone();
-        for (_, offset) in &mut departing_offsets {
-            *offset -= highest_bus_offset as isize;
-        }
-
-        for n in (0isize..).step_by(*highest_bus_id) {
-            if departing_offsets
-                .iter()
-                .all(|(bus_id, offset)| (n + offset) % *bus_id as isize == 0)
-            {
-                return Some(n + departing_offsets[0].1);
-            }
-        }
-
-        None
-    }
+    (timestamp, schedule)
 }
 
 fn main() {
@@ -86,11 +63,11 @@ fn main() {
         process::exit(1);
     }
 
-    let bus_schedule = BusSchedule::from_file(env::args().nth(1).unwrap());
-    let part1 = bus_schedule.part1();
-    let part2_brute_force = bus_schedule.part2_brute_force();
+    let (timestamp, schedule) = parse_input(env::args().nth(1).unwrap());
+    let part1 = solve_part1(timestamp, &schedule);
+    let part2 = solve_part2_brute_force(&schedule);
     println!("Result (Part 1): {}", part1);
-    println!("Result (Part 2): {:?}", part2_brute_force);
+    println!("Result (Part 2): {:?}", part2);
 }
 
 #[cfg(test)]
@@ -99,44 +76,44 @@ mod tests {
 
     #[test]
     fn test_example_input_1() {
-        let bus_schedule = BusSchedule::from_file("example1.txt");
-        assert_eq!(bus_schedule.part1(), 295);
-        assert_eq!(bus_schedule.part2_brute_force(), Some(1068781));
+        let (timestamp, schedule) = parse_input("example1.txt");
+        assert_eq!(solve_part1(timestamp, &schedule), 295);
+        assert_eq!(solve_part2_brute_force(&schedule), Some(1068781));
     }
 
     #[test]
     fn test_example_input_2() {
-        let bus_schedule = BusSchedule::from_file("example2.txt");
-        assert_eq!(bus_schedule.part2_brute_force(), Some(3417));
+        let (_, schedule) = parse_input("example2.txt");
+        assert_eq!(solve_part2_brute_force(&schedule), Some(3417));
     }
 
     #[test]
     fn test_example_input_3() {
-        let bus_schedule = BusSchedule::from_file("example3.txt");
-        assert_eq!(bus_schedule.part2_brute_force(), Some(754018));
+        let (_, schedule) = parse_input("example3.txt");
+        assert_eq!(solve_part2_brute_force(&schedule), Some(754018));
     }
 
     #[test]
     fn test_example_input_4() {
-        let bus_schedule = BusSchedule::from_file("example4.txt");
-        assert_eq!(bus_schedule.part2_brute_force(), Some(779210));
+        let (_, schedule) = parse_input("example4.txt");
+        assert_eq!(solve_part2_brute_force(&schedule), Some(779210));
     }
 
     #[test]
     fn test_example_input_5() {
-        let bus_schedule = BusSchedule::from_file("example5.txt");
-        assert_eq!(bus_schedule.part2_brute_force(), Some(1261476));
+        let (_, schedule) = parse_input("example5.txt");
+        assert_eq!(solve_part2_brute_force(&schedule), Some(1261476));
     }
 
     #[test]
     fn test_example_input_6() {
-        let bus_schedule = BusSchedule::from_file("example6.txt");
-        assert_eq!(bus_schedule.part2_brute_force(), Some(1202161486));
+        let (_, schedule) = parse_input("example6.txt");
+        assert_eq!(solve_part2_brute_force(&schedule), Some(1202161486));
     }
 
     #[test]
     fn test_puzzle_input() {
-        let bus_schedule = BusSchedule::from_file("input.txt");
-        assert_eq!(bus_schedule.part1(), 115);
+        let (timestamp, schedule) = parse_input("input.txt");
+        assert_eq!(solve_part1(timestamp, &schedule), 115);
     }
 }
