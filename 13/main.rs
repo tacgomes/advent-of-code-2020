275e6f1ce7306e63c0071f86cc3e5add186c8676
Tiago Gomes <tacg@tacgomes.com>
2021-01-11 23:50:05 +0000

    Day 13: use optimised solution for part 2

--- 13/main.rs
+++ 13/main.rs
@@ -3,44 +3,36 @@ use std::fs;
 use std::path::Path;
 use std::process;
 
-fn solve_part1(timestamp: usize, schedule: &[(usize, isize)]) -> usize {
+fn solve_part1(timestamp: usize, schedule: &[(usize, usize)]) -> usize {
     let mut timestamps = schedule
         .iter()
         .map(|(x, _)| (x, (x - (timestamp % x) % x) + timestamp))
         .collect::<Vec<_>>();
+
     timestamps.sort_by_key(|&(_, ts)| ts);
-    (timestamps[0].1 - timestamp) * timestamps[0].0
+    (timestamps[0].1 - timestamp) * timestamps[0].0)
 }
 
-fn solve_part2_brute_force(schedule: &[(usize, isize)]) -> Option<isize> {
-    // TODO: add an implementation of part 2 using the much more
-    // efficient Chinese Remainder Theorem.
+fn solve_part2(schedule: &[(usize, usize)]) -> usize {
+    /* The algorithm assumes that all the bus ids are co-prime, and as
+     * that implies that gcd(bus1, bus2) is 1, we can derive lcm(bus1,
+     * bus2) by simply multiplying the bus ids together: bus1 * bus2.
+     */
+    let mut num = 0;
+    let mut step = schedule[0].0;
+    let mut rem = &schedule[1..];
 
-    let highest_bus_id = schedule.iter().map(|(x, _)| x).max().unwrap();
-    let highest_bus_offset = schedule
-        .iter()
-        .find(|(x, _)| x == highest_bus_id)
-        .unwrap()
-        .1;
-
-    let mut schedule = schedule.to_owned();
-    for (_, offset) in schedule.iter_mut() {
-        *offset -= highest_bus_offset as isize;
+    while !rem.is_empty() {
+        let (bus_id, distance) = rem[0];
+        num = (num..).step_by(step).find(|x| (x + distance) % bus_id == 0).unwrap();
+        step *= bus_id;
+        rem = &rem[1..];
     }
 
-    for n in (0isize..).step_by(*highest_bus_id) {
-        if schedule
-            .iter()
-            .all(|(bus_id, offset)| (n + offset) % *bus_id as isize == 0)
-        {
-            return Some(n + schedule[0].1);
-        }
-    }
-
-    None
+    num
 }
 
-fn parse_input(file_name: impl AsRef<Path>) -> (usize, Vec<(usize, isize)>) {
+fn parse_input(file_name: impl AsRef<Path>) -> (usize, Vec<(usize, usize)>) {
     let content = fs::read_to_string(&file_name).unwrap();
     let mut lines = content.lines();
 
@@ -51,7 +43,7 @@ fn parse_input(file_name: impl AsRef<Path>) -> (usize, Vec<(usize, isize)>) {
         .split(',')
         .enumerate()
         .filter(|(_, b)| !b.starts_with('x'))
-        .map(|(i, b)| (b.parse().unwrap(), i as isize))
+        .map(|(i, b)| (b.parse().unwrap(), i as usize))
         .collect();
 
     (timestamp, schedule)
@@ -65,7 +57,7 @@ fn main() {
 
     let (timestamp, schedule) = parse_input(env::args().nth(1).unwrap());
     let part1 = solve_part1(timestamp, &schedule);
-    let part2 = solve_part2_brute_force(&schedule);
+    let part2 = solve_part2(&schedule);
     println!("Result (Part 1): {}", part1);
     println!("Result (Part 2): {:?}", part2);
 }
@@ -78,42 +70,43 @@ mod tests {
     fn test_example_input_1() {
         let (timestamp, schedule) = parse_input("example1.txt");
         assert_eq!(solve_part1(timestamp, &schedule), 295);
-        assert_eq!(solve_part2_brute_force(&schedule), Some(1068781));
+        assert_eq!(solve_part2(&schedule), 1068781);
     }
 
     #[test]
     fn test_example_input_2() {
         let (_, schedule) = parse_input("example2.txt");
-        assert_eq!(solve_part2_brute_force(&schedule), Some(3417));
+        assert_eq!(solve_part2(&schedule), 3417);
     }
 
     #[test]
     fn test_example_input_3() {
         let (_, schedule) = parse_input("example3.txt");
-        assert_eq!(solve_part2_brute_force(&schedule), Some(754018));
+        assert_eq!(solve_part2(&schedule), 754018);
     }
 
     #[test]
     fn test_example_input_4() {
         let (_, schedule) = parse_input("example4.txt");
-        assert_eq!(solve_part2_brute_force(&schedule), Some(779210));
+        assert_eq!(solve_part2(&schedule), 779210);
     }
 
     #[test]
     fn test_example_input_5() {
         let (_, schedule) = parse_input("example5.txt");
-        assert_eq!(solve_part2_brute_force(&schedule), Some(1261476));
+        assert_eq!(solve_part2(&schedule), 1261476);
     }
 
     #[test]
     fn test_example_input_6() {
         let (_, schedule) = parse_input("example6.txt");
-        assert_eq!(solve_part2_brute_force(&schedule), Some(1202161486));
+        assert_eq!(solve_part2(&schedule), 1202161486);
     }
 
     #[test]
     fn test_puzzle_input() {
         let (timestamp, schedule) = parse_input("input.txt");
         assert_eq!(solve_part1(timestamp, &schedule), 115);
+        assert_eq!(solve_part2(&schedule), 756261495958122);
     }
 }
