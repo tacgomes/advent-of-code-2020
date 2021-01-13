4ffa8beb52d05d956f7188bdef5fb06b362e1c58
Tiago Gomes <tacg@tacgomes.com>
2021-01-13 19:28:47 +0000

    Day 15: improve solution

--- 15/main.rs
+++ 15/main.rs
@@ -1,23 +1,17 @@
 use std::collections::HashMap;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 use std::path::Path;
 use std::process;
 
-fn calculate_num(file_name: impl AsRef<Path>, target_turn: usize) -> usize {
-    let mut file = File::open(file_name).unwrap();
-    let mut numbers = String::new();
-    file.read_to_string(&mut numbers).unwrap();
+fn solve(numbers: &[usize], target_turn: usize) -> usize {
+    let mut spoken_numbers = numbers
+        .iter()
+        .enumerate()
+        .map(|(turn, num)| (*num, turn + 1))
+        .collect::<HashMap<_, _>>();
 
-    let mut spoken_numbers = HashMap::new();
-    let mut last_turn_number = 0;
-
-    for (turn, num) in numbers.trim().split(',').enumerate() {
-        last_turn_number = num.parse::<usize>().unwrap();
-        spoken_numbers.insert(last_turn_number, turn + 1);
-    }
-
+    let mut last_turn_number = *numbers.last().unwrap();
     for turn in spoken_numbers.len()..target_turn {
         let this_turn_number = match spoken_numbers.get(&last_turn_number) {
             None => 0,
@@ -30,15 +24,24 @@ fn calculate_num(file_name: impl AsRef<Path>, target_turn: usize) -> usize {
     last_turn_number
 }
 
+fn parse_input(file_name: impl AsRef<Path>) -> Vec<usize> {
+    fs::read_to_string(&file_name)
+        .unwrap()
+        .trim()
+        .split(',')
+        .map(|x| x.parse().unwrap())
+        .collect()
+}
+
 fn main() {
     if env::args().count() != 3 {
         eprintln!("USAGE: {} FILE TURN", env::args().next().unwrap());
         process::exit(1);
     }
 
-    let file_name = env::args().nth(1).unwrap();
+    let numbers = parse_input(env::args().nth(1).unwrap());
     let target_turn = env::args().nth(2).unwrap().parse().unwrap();
-    let result = calculate_num(file_name, target_turn);
+    let result = solve(&numbers, target_turn);
     println!("Result: {}", result);
 }
 
@@ -48,41 +51,49 @@ mod tests {
 
     #[test]
     fn test_example_input_1() {
-        assert_eq!(calculate_num("example1.txt", 2020), 436);
+        let numbers = parse_input("example1.txt");
+        assert_eq!(solve(&numbers, 2020), 436);
     }
 
     #[test]
     fn test_example_input_2() {
-        assert_eq!(calculate_num("example2.txt", 2020), 1);
+        let numbers = parse_input("example2.txt");
+        assert_eq!(solve(&numbers, 2020), 1);
     }
 
     #[test]
     fn test_example_input_3() {
-        assert_eq!(calculate_num("example3.txt", 2020), 10);
+        let numbers = parse_input("example3.txt");
+        assert_eq!(solve(&numbers, 2020), 10);
     }
 
     #[test]
     fn test_example_input_4() {
-        assert_eq!(calculate_num("example4.txt", 2020), 27);
+        let numbers = parse_input("example4.txt");
+        assert_eq!(solve(&numbers, 2020), 27);
     }
 
     #[test]
     fn test_example_input_5() {
-        assert_eq!(calculate_num("example5.txt", 2020), 78);
+        let numbers = parse_input("example5.txt");
+        assert_eq!(solve(&numbers, 2020), 78);
     }
 
     #[test]
     fn test_example_input_6() {
-        assert_eq!(calculate_num("example6.txt", 2020), 438);
+        let numbers = parse_input("example6.txt");
+        assert_eq!(solve(&numbers, 2020), 438);
     }
 
     #[test]
     fn test_example_input_7() {
-        assert_eq!(calculate_num("example7.txt", 2020), 1836);
+        let numbers = parse_input("example7.txt");
+        assert_eq!(solve(&numbers, 2020), 1836);
     }
 
     #[test]
     fn test_puzzle_input() {
-        assert_eq!(calculate_num("input.txt", 2020), 929);
+        let numbers = parse_input("input.txt");
+        assert_eq!(solve(&numbers, 2020), 929);
     }
 }
