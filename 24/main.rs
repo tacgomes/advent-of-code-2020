96ff8f77cd5ecf901b32eb8c4c07226b2778dc64
Tiago Gomes <tacg@tacgomes.com>
2021-01-17 12:19:24 +0000

    Day 24: improve solution

--- 24/main.rs
+++ 24/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
 use std::fs;
@@ -6,82 +7,79 @@ use std::process;
 
 const NEIGHBORS: [(isize, isize); 6] = [(-1, 1), (1, 1), (2, 0), (1, -1), (-1, -1), (-2, 0)];
 
-fn get_black_tiles(file_name: impl AsRef<Path>) -> HashSet<(isize, isize)> {
-    let content = fs::read_to_string(file_name).unwrap();
-    let mut black_tiles = HashSet::new();
+type Tiles = HashSet<(isize, isize)>;
 
+fn iterate_once(tiles: Tiles) -> Tiles {
+    let mut counter = HashMap::new();
+
+    tiles
+        .iter()
+        .flat_map(|(x, y)| {
+            NEIGHBORS
+                .iter()
+                .map(move |neigh| (x + neigh.0, y + neigh.1))
+        })
+        .for_each(|(x, y)| *counter.entry((x, y)).or_insert(0) += 1);
+
+    counter
+        .iter()
+        .filter(|&(k, &c)| (c == 2 || (c == 1 && tiles.contains(&k))))
+        .map(|(k, _)| k)
+        .cloned()
+        .collect()
+}
+
+fn iterate(mut tiles: Tiles) -> Tiles {
+    for _ in 0..100 {
+        tiles = iterate_once(tiles);
+    }
+    tiles
+}
+
+fn parse_input(file_name: impl AsRef<Path>) -> Tiles {
+    let content = fs::read_to_string(file_name).unwrap();
+    let mut tiles = HashSet::new();
+
+    /*
+     * Use double coordinates to represent the hexagonal grid as
+     * explained here:
+     * https://www.redblobgames.com/grids/hexagons/#coordinates-doubled
+     */
     for line in content.lines() {
-        let mut x = 0;
-        let mut y = 0;
-        let mut iter = line.chars();
+        let (mut x, mut y, mut iter) = (0, 0, line.chars());
         while let Some(c) = iter.next() {
-            match c {
-                'e' => x += 2,
-                'w' => x -= 2,
-                'n' => {
+            let mut coord = c.to_string();
+            if c == 'n' || c == 's' {
+                coord.push(iter.next().unwrap());
+            }
+            match coord.as_str() {
+                "nw" => {
+                    x -= 1;
                     y += 1;
-                    match iter.next().unwrap() {
-                        'e' => x += 1,
-                        'w' => x -= 1,
-                        _ => unreachable!(),
-                    }
                 }
-                's' => {
+                "ne" => {
+                    x += 1;
+                    y += 1;
+                }
+                "e" => x += 2,
+                "se" => {
+                    x += 1;
                     y -= 1;
-                    match iter.next().unwrap() {
-                        'e' => x += 1,
-                        'w' => x -= 1,
-                        _ => unreachable!(),
-                    }
                 }
+                "sw" => {
+                    x -= 1;
+                    y -= 1;
+                }
+                "w" => x -= 2,
                 _ => unreachable!(),
             }
         }
-        if !black_tiles.remove(&(x, y)) {
-            black_tiles.insert((x, y));
+        if !tiles.remove(&(x, y)) {
+            tiles.insert((x, y));
         }
     }
 
-    black_tiles
-}
-
-fn caculate_part2_black_tiles_count(mut black_tiles: HashSet<(isize, isize)>) -> usize {
-    for _ in 0..100 {
-        let mut new_black_tiles = HashSet::new();
-        let mut white_tiles = HashSet::new();
-
-        for (x, y) in &black_tiles {
-            let mut num_black_tiles = 0;
-            for neigh in &NEIGHBORS {
-                let (neigh_x, neigh_y) = (x + neigh.0, y + neigh.1);
-                if black_tiles.contains(&(neigh_x, neigh_y)) {
-                    num_black_tiles += 1;
-                } else {
-                    white_tiles.insert((neigh_x, neigh_y));
-                }
-            }
-            if !(num_black_tiles == 0 || num_black_tiles > 2) {
-                new_black_tiles.insert((*x, *y));
-            }
-        }
-
-        for (x, y) in &white_tiles {
-            let mut num_black_tiles = 0;
-            for neigh in &NEIGHBORS {
-                let (neigh_x, neigh_y) = (x + neigh.0, y + neigh.1);
-                if black_tiles.contains(&(neigh_x, neigh_y)) {
-                    num_black_tiles += 1;
-                }
-            }
-            if num_black_tiles == 2 {
-                new_black_tiles.insert((*x, *y));
-            }
-        }
-
-        black_tiles = new_black_tiles;
-    }
-
-    black_tiles.len()
+    tiles
 }
 
 fn main() {
@@ -90,11 +88,12 @@ fn main() {
         process::exit(1);
     }
 
-    let black_tiles = get_black_tiles(env::args().nth(1).unwrap());
-    let part1_count = black_tiles.len();
-    let part2_count = caculate_part2_black_tiles_count(black_tiles);
-    println!("Result (Part 1): {}", part1_count);
-    println!("Result (Part 2): {}", part2_count);
+    let tiles1 = parse_input(env::args().nth(1).unwrap());
+    let tiles2 = iterate(tiles1.clone());
+    let part1 = tiles1.len();
+    let part2 = tiles2.len();
+    println!("Result (Part 1): {}", part1);
+    println!("Result (Part 2): {}", part2);
 }
 
 #[cfg(test)]
@@ -103,15 +102,17 @@ mod tests {
 
     #[test]
     fn test_example_input() {
-        let black_tiles = get_black_tiles("example.txt");
-        assert_eq!(black_tiles.len(), 10);
-        assert_eq!(caculate_part2_black_tiles_count(black_tiles), 2208);
+        let tiles1 = parse_input("example.txt");
+        let tiles2 = iterate(tiles1.clone());
+        assert_eq!(tiles1.len(), 10);
+        assert_eq!(tiles2.len(), 2208);
     }
 
     #[test]
     fn test_puzzle_input() {
-        let black_tiles = get_black_tiles("input.txt");
-        assert_eq!(black_tiles.len(), 512);
-        assert_eq!(caculate_part2_black_tiles_count(black_tiles), 4120);
+        let tiles1 = parse_input("input.txt");
+        let tiles2 = iterate(tiles1.clone());
+        assert_eq!(tiles1.len(), 512);
+        assert_eq!(tiles2.len(), 4120);
     }
 }
